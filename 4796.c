bed3cb1e983384b7087bd6f55745197f7aa42ca9
kimjunegeun <junegeun.kim@gmail.com>
2020-01-16 10:57:24 +0900

    20/01/16 4796번

--- 4796.c
+++ 4796.c
@@ -1,5 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    
+    int L, P, V;
+    int cnt = 0, use;
+    for (;;)
+    {
+        use = 0;
+        scanf("%d %d %d", &L, &P, &V);
+        if (L == 0 && P == 0 && V == 0)
+        {
+            break;
+        }
+        cnt++;
+        while(1) {
+            if(V<L) {
+                use+=V;
+                break;
+            }
+            V-=L;
+            use+=L;
+            V-=(P-L);
+            if(V<=0) {
+                break;
+            }
+
+        }
+        printf("Case %d: %d\n",cnt, use);
+    }
+    return 0;
 }
+
+       
